6c33ff3630c6b7b5216773db43b28d609d3ed08f
unknown <iddy2525@icloud.com>
2024-02-28 16:43:28 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,13 @@
+# A code for taking user input, coded by using python programing language.
+
+# Here weaAsk the users for their name
+name = input("What is your name?: ")
+
+# Here we ask the users for their age
+age = input("How old are you?: ")
+
+# Here we ask the user for their location
+location = input("Where do you live?: ")
+
+# Here we print out a magic a personalized message
+print("Hello " + name + ", you are " + age + " years old and live in " + location + ".")
